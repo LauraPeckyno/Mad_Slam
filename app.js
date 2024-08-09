08237e95afc9cb1288e490d9b5cb5e4c5fff6f37
LauraP <166152671+LauraPeckyno@users.noreply.github.com>
2024-08-09 14:41:19 -0400

    tested. initial word collection form.

---
+++ app.js
@@ -0,0 +1,17 @@
+document.getElementById('wordForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const adjective1 = document.getElementById('adjective1').value;
+    const noun1 = document.getElementById('noun1').value;
+    const noun2 = document.getElementById('noun2').value;
+    const verb1 = document.getElementById('verb1').value;
+    const verb2 = document.getElementById('verb2').value;
+
+    const poem = `
+        The ${adjective1} ${noun1}<br>
+        ${verb1} across the ${noun2}<br>
+        ${adjective1} ${noun1} ${verb2}
+    `;
+
+    document.getElementById('wordsOutput').innerHTML = poem;
+});
